7df47c941ae7beaf0bfea0a564a253035de49a1f
Andrei Yeutukhou <gavnorock84@hotmail.com>
2025-01-19 13:27:22 +0300

    add theme switcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
+import ThemeSwitcher from './components/ThemeSwitcher/ThemeSwitcher.tsx'
+
 import './App.scss'
 
 export const App = () => {
 
   return (
     <>
+      <ThemeSwitcher />
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
