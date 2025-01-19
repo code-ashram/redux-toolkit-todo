85e4c847c55badcff2ccde38d9e24a6be87091ef
Andrei Yeutukhou <gavnorock84@hotmail.com>
2025-01-19 13:06:06 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.scss'
+
+export const App = () => {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
